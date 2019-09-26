26a93bd70ea69d736bdeb02f8f822f91a201580b
aliciaguzmanmarin <juliagm999@gmail.com>
2019-09-26 18:48:59 -0400

    Controlador enlazado con la base de datos

---
+++ webEval/webEval/Models/Data Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace webEval.Models
+{
+    public class Data_Context : DbContext
+
+    {
+
+        public Data_Context() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<webEval.Models.Student> Students { get; set; }
+    }
+}
